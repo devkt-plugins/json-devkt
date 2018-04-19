a3078979caf603d6342a5215b819b6a963cbfccc
ice1000 <ice1000kotlin@foxmail.com>
2018-04-20 02:10:32 +0800

    Update highlighter

--- src/com/intellij/devkt/json/Json.java
+++ src/com/intellij/devkt/json/Json.java
@@ -1,12 +1,10 @@
 package com.intellij.devkt.json;
 
-import org.ice1000.devkt.openapi.AnnotationHolder;
 import org.ice1000.devkt.openapi.ColorScheme;
 import org.ice1000.devkt.openapi.ExtendedDevKtLanguage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement;
-import org.jetbrains.kotlin.com.intellij.psi.PsiErrorElement;
+import org.jetbrains.kotlin.com.intellij.psi.StringEscapesTokenTypes;
 import org.jetbrains.kotlin.com.intellij.psi.tree.IElementType;
 
 import javax.swing.*;
@@ -34,11 +32,6 @@ public class Json<T> extends ExtendedDevKtLanguage<T> {
 	}
 
 	@Override
-	public void annotate(PsiElement psiElement, AnnotationHolder<? super T> annotationHolder, ColorScheme<? extends T> colorScheme) {
-		if (psiElement instanceof PsiErrorElement) annotationHolder.highlight(psiElement, colorScheme.getUnknown());
-	}
-
-	@Override
 	public @Nullable
 	T attributesOf(IElementType iElementType, ColorScheme<? extends T> colorScheme) {
 		if (iElementType == JsonElementTypes.LINE_COMMENT) return colorScheme.getLineComments();
@@ -46,10 +39,13 @@ public class Json<T> extends ExtendedDevKtLanguage<T> {
 		else if (iElementType == JsonElementTypes.NUMBER) return colorScheme.getNumbers();
 		else if (iElementType == JsonElementTypes.SINGLE_QUOTED_STRING) return colorScheme.getString();
 		else if (iElementType == JsonElementTypes.DOUBLE_QUOTED_STRING) return colorScheme.getProperty();
+		else if (iElementType == StringEscapesTokenTypes.VALID_STRING_ESCAPE_TOKEN) return colorScheme.getStringEscape();
 		else if (iElementType == JsonElementTypes.IDENTIFIER) return colorScheme.getIdentifiers();
+		else if (iElementType == JsonElementTypes.COLON) return colorScheme.getColon();
+		else if (iElementType == JsonElementTypes.COMMA) return colorScheme.getComma();
 		else if (JsonParserDefinition.JSON_BRACES.contains(iElementType)) return colorScheme.getBraces();
 		else if (JsonParserDefinition.JSON_KEYWORDS.contains(iElementType)) return colorScheme.getKeywords();
 		else if (JsonParserDefinition.JSON_BRACKETS.contains(iElementType)) return colorScheme.getBrackets();
-		else return null;
+		else return super.attributesOf(iElementType, colorScheme);
 	}
 }
