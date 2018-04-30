936662bb3332a00cef8574e31e604783bd09de02
ice1000 <ice1000kotlin@foxmail.com>
2018-04-30 13:28:27 +0800

    Migrate

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import java.io.*
 
-val kotlinVersion = "1.2.40"
+val kotlinVersion = "1.2.41"
 
 group = "com.intellij.devkt.json"
 version = "v1.0"
@@ -34,7 +34,7 @@ application {
 
 dependencies {
 	compileOnly(kotlin("compiler-embeddable", kotlinVersion))
-	val version = "v1.4"
+	val version = "v1.4.1"
 	compileOnly(group = "com.github.ice1000.dev-kt", name = "common", version = version)
 	runtime(group = "com.github.ice1000.dev-kt", name = "swing", version = version)
 }
